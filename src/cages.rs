a9609b1edd9b98291a34ac25d77c111edab91efd
peterthenelson <nelson.peter.a@gmail.com>
2025-05-07 00:04:37 -0400

    Add cage strat

--- src/cages.rs
+++ src/cages.rs
@@ -1,8 +1,9 @@
-use crate::{dfs::{Constraint, ConstraintResult, ConstraintViolationDetail}, sudoku::{Sudoku, SudokuAction}};
+use crate::{dfs::{Constraint, ConstraintResult, ConstraintViolationDetail, PartialStrategy}, sudoku::{Sudoku, SudokuAction}};
 
 pub struct Cage {
     pub cells: Vec<(usize, usize)>,
     pub target: u8,
+    // TODO: Add option for exclusivity
 }
 
 impl Cage {
@@ -70,6 +71,42 @@ Constraint<SudokuAction<MIN, MAX>, Sudoku<N, M, MIN, MAX>> for CageChecker {
     }
 }
 
+pub struct CagePartialStrategy {
+    cages: Vec<Cage>,
+}
+
+impl <const MIN: u8, const MAX: u8, const N: usize, const M: usize>
+PartialStrategy<SudokuAction<MIN, MAX>, Sudoku<N, M, MIN, MAX>> for CagePartialStrategy {
+    // TODO: Give exact value if it's the last cell in the cage.
+    // TODO: Update the strategy for cages that are exclusive.
+    fn suggest(&self, puzzle: &Sudoku<N, M, MIN, MAX>) -> Result<Vec<SudokuAction<MIN, MAX>>, crate::dfs::PuzzleError> {
+        for cage in &self.cages {
+            let mut sum = 0;
+            let mut first_empty: Option<(usize, usize)> = None;
+            for cell in &cage.cells {
+                match puzzle.grid[cell.0][cell.1] {
+                    Some(value) => sum += value,
+                    None => {
+                        if first_empty.is_none() {
+                            first_empty = Some(cell.clone());
+                        }
+                    }
+                }
+            }
+            if let Some(empty) = first_empty {
+                let target = cage.get_target();
+                let remaining = target - sum;
+                return Ok((MIN..(std::cmp::min(remaining, MAX) + 1)).map(|value| SudokuAction {
+                    row: empty.0,
+                    col: empty.1,
+                    value,
+                }).collect::<Vec<SudokuAction<MIN, MAX>>>());
+            }
+        }
+        Ok(vec![])
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::vec;
@@ -120,5 +157,23 @@ mod tests {
                 ]),
             },
         ]));
+    }
+
+    #[test]
+    fn test_cage_partial_strategy() {
+        let cage_strategy = CagePartialStrategy {
+            cages: vec![Cage::new(vec![(0, 0), (0, 1)], 4)],
+        };
+        let puzzle = Sudoku::<4, 4, 1, 4>::parse(
+            "2...\n\
+             ....\n\
+             ....\n\
+             ....\n"
+        ).unwrap();
+        let actions = cage_strategy.suggest(&puzzle).unwrap();
+        assert_eq!(actions, vec![
+            SudokuAction { row: 0, col: 1, value: 1 },
+            SudokuAction { row: 0, col: 1, value: 2 },
+        ]);
     }
 }
