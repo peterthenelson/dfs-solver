16811cf1c1046eaac3a409278fe24cd3fe819b31
peterthenelson <nelson.peter.a@gmail.com>
2025-06-21 12:35:00 -0400

    Stats snapshot script

---
+++ variant-sudoku-puzzles/scripts/snapshot-stats.sh
@@ -0,0 +1,12 @@
+set -euo pipefail
+git add .
+hash=$(git describe --always --dirty)
+mkdir "figures/$hash"
+echo "Stats for commit $hash" >> "figures/$hash/summary.txt"
+echo "----------------------" >> "figures/$hash/summary.txt"
+cargo build --release
+for puzzle in variant-sudoku-puzzles/src/bin/*.rs; do
+  puzzle=$(basename $puzzle .rs)
+  cargo run --release --bin $puzzle 1>> "figures/$hash/summary.txt"
+  mv "figures/$puzzle.png" "figures/$hash"
+done
