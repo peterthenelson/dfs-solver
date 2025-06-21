b9a4dfb4efa574214969619a6cf9d135c42648af
peterthenelson <nelson.peter.a@gmail.com>
2025-06-21 12:36:55 -0400

    Formatting tweak

--- variant-sudoku-puzzles/scripts/snapshot-stats.sh
+++ variant-sudoku-puzzles/scripts/snapshot-stats.sh
@@ -3,7 +3,7 @@ git add .
 hash=$(git describe --always --dirty)
 mkdir "figures/$hash"
 echo "Stats for commit $hash" >> "figures/$hash/summary.txt"
-echo "----------------------" >> "figures/$hash/summary.txt"
+echo "------------------------" >> "figures/$hash/summary.txt"
 cargo build --release
 for puzzle in variant-sudoku-puzzles/src/bin/*.rs; do
   puzzle=$(basename $puzzle .rs)
